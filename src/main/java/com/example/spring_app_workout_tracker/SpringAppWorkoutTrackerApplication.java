96be2de2f2b3cc3654c9ab8ed5bff7b730d8a2a2
Vlad-Andrei Cojocariu <vlad-andrei.cojocariu@ibm.com>
2025-04-11 15:18:28 +0300

    HWVE-0000 working app

---
+++ src/main/java/com/example/spring_app_workout_tracker/SpringAppWorkoutTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_app_workout_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppWorkoutTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppWorkoutTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_app_workout_tracker/SpringAppWorkoutTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_app_workout_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppWorkoutTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
